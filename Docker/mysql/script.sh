6863740e7aa93d8d1a14283b866970240169beb6
Artak <Artak.15@outlook.com>
2022-06-21 02:47:51 -0700

    fix cron script

--- Docker/mysql/script.sh
+++ Docker/mysql/script.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# ┌───────────── minute (0 - 59)
-# │     ┌───────────── hour (0 - 23)
-# │     │     ┌───────────── day of the month (1 - 31)
-# │     │     │   ┌───────────── month (1 - 12)
-# │     │     │   │   ┌───────────── day of the week (0 - 6) (Sunday to Saturday;
-# │     │     │   │   │                                   7 is also Sunday on some systems)
-# │     │     │   │   │
-# │     │     │   │   │
-# *     *     *   *   *     <command to execute>
-
 mysql_password=&quot;guntantin;
 mysql_user=&quot;root&quot;
 mysql_port=&quot;3306&quot;
